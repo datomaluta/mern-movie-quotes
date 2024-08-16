6955844a8a8309500dbe020d08c59d4d726c67eb
datomaluta <datomaluta@gmail.com>
2024-08-16 16:49:44 +0400

    fix: user update issue - remove isDirty and user state

--- client/src/components/profile/GeneralInfoForm.tsx
+++ client/src/components/profile/GeneralInfoForm.tsx
@@ -5,7 +5,7 @@ import CustomInput from "../ui/customInputs/CustomInput";
 import { useTranslate } from "../../hooks/useTranslate";
 import useUploadImage from "../../hooks/useUploadImage";
 import { app } from "../../firebase.ts";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import ImagePicker from "./ImagePicker.tsx";
 import { useSearchParams } from "react-router-dom";
@@ -34,6 +34,7 @@ const GeneralInfoForm = () => {
   const { currentUser } = useSelector((state: RootState) => state.user);
   const [, setSearchParams] = useSearchParams();
   const dispatch = useDispatch();
+  const [saveChangeIsVisible, setSaveChangeIsVisible] = useState(false);
 
   const {
     uploadImage,
@@ -47,7 +48,7 @@ const GeneralInfoForm = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors, isDirty },
+    formState: { errors },
     control,
     reset,
   } = useForm<FormData>({
@@ -58,12 +59,27 @@ const GeneralInfoForm = () => {
     },
   });
   const imageInput = useWatch({ control, name: "image" });
+  const usernameInput = useWatch({ control, name: "username" });
+  const emailInput = useWatch({ control, name: "email" });
+
+  useEffect(() => {
+    if (
+      usernameInput !== currentUser?.username ||
+      emailInput !== currentUser?.email ||
+      imageInput !== null
+    ) {
+      setSaveChangeIsVisible(true);
+    } else {
+      setSaveChangeIsVisible(false);
+    }
+  }, [usernameInput, emailInput, imageInput, currentUser]);
 
   const { mutate, isPending } = useMutation({
     mutationFn: updateMe,
     onSuccess: (response: { data: { data: { user: UserType } } }) => {
       toast.success(t("profile_updated_successfully"));
       dispatch(saveUserInfo(response?.data?.data?.user));
+      setSaveChangeIsVisible(false);
     },
     onError: (error: { response: { data: { message: string } } }) => {
       toast.error(error?.response?.data?.message);
@@ -92,83 +108,6 @@ const GeneralInfoForm = () => {
       onSubmit={handleSubmit(submitHandler)}
     >
       <div className="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-[40%]">
-        {/* <div className="flex flex-col items-center gap-2">
-          <div className="w-[11.75rem]  h-[11.75rem]  rounded-full overflow-hidden relative bg-project-light-blue">
-            {imageFileUploadProgress ? (
-              <CircularProgressbar
-                value={imageFileUploadProgress || 0}
-                text={`${imageFileUploadProgress}%`}
-                strokeWidth={5}
-                styles={{
-                  root: {
-                    width: "100%",
-                    height: "100%",
-                    position: "absolute",
-                    top: 0,
-                    left: 0,
-                    zIndex: 100,
-                  },
-                  path: {
-                    stroke: `rgba(62, 152, 199, ${
-                      imageFileUploadProgress / 100
-                    })`,
-                  },
-                }}
-              />
-            ) : (
-              ""
-            )}
-            <img
-              className={`w-[11.75rem]  h-[11.75rem] object-cover   ${
-                imageFileUploadProgress &&
-                imageFileUploadProgress < 100 &&
-                "opacity-60"
-              }`}
-              src={imgPreview || currentUser?.image}
-              alt="img-preview"
-            />
-          </div>
-
-          <label className="cursor-pointer" htmlFor="image">
-            {t("upload_new_photo")}
-          </label>
-          <input
-            {...register("image", {
-              onChange: hanldeImageChange,
-            })}
-            type="file"
-            id="image"
-            hidden
-          />
-
-          <div className="flex gap-4 min-h-8">
-            {imageInput !== currentUser?.image &&
-              imageUploadButtonIsVisible && (
-                <>
-                  <button
-                    onClick={() => {
-                      setValue("image", currentUser?.image);
-                      setImgPreview(null);
-                    }}
-                  >
-                    {t("cancel")}
-                  </button>
-                  <button
-                    onClick={() => {
-                      uploadImage(imageInput[0]);
-                      setImageUploadButtonIsVisible(false);
-                    }}
-                    className="bg-green-600 px-3 py-1 rounded-lg"
-                  >
-                    {t("upload")}
-                  </button>
-                </>
-              )}
-          </div>
-          {imageFileUploadError && (
-            <p className="text-project-red">{imageFileUploadError}</p>
-          )}
-        </div> */}
         <ImagePicker
           imageFileUploadProgress={imageFileUploadProgress}
           register={register}
@@ -225,7 +164,7 @@ const GeneralInfoForm = () => {
         </button>
       )}
 
-      {isDirty && (
+      {saveChangeIsVisible && (
         <div className="flex gap-6 mt-16 justify-end">
           <button
             onClick={() => {
