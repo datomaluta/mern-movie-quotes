9744bcbffd0d3b9061584f55f03d61bc7fdef0a5
datomaluta <datomaluta@gmail.com>
2024-08-16 20:17:02 +0400

    deployment: update vite env and tsconfig

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string; // Add your environment variables here
+  // readonly VITE_ANOTHER_VAR: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
