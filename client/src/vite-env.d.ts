6aabc84e8ed64324643d55d5fe3d12f5955bcbd3
datomaluta <datomaluta@gmail.com>
2024-08-16 20:21:12 +0400

    deployment: add custom.d.ts file

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,6 +1,9 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
-  readonly VITE_API_URL: string; // Add your environment variables here
+  readonly VITE_API_URL: string;
+  readonly VITE_SOCKET_URL: string;
+  readonly VITE_FIREBASE_API_KEY: string;
+  // Add your environment variables here
   // readonly VITE_ANOTHER_VAR: string;
 }
 
